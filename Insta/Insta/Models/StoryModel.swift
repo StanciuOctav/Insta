c3b552985e99334307d7f11ed99f87b22ccb4f7b
StanciuOctav <octav.stanciu00@gmail.com>
2025-07-27 18:24:28 +0300

    Created the model for the JSON file

---
+++ Insta/Insta/Models/StoryModel.swift
@@ -0,0 +1,31 @@
+//
+//  StoryModel.swift
+//  Insta
+//
+//  Created by Octav Stanciu on 27.07.2025.
+//
+
+import Foundation
+
+struct StoriesRoot: Codable {
+    let pages: [StoriesPage]
+}
+
+struct StoriesPage: Codable {
+    let users: [StoryUser]
+}
+
+struct StoryUser: Codable, Identifiable, Equatable {
+    let id: Int
+    let name: String
+    let profilePicUrl: String
+    
+    var isSeen: Bool = false
+    var isLiked: Bool = false
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case profilePicUrl = "profile_picture_url"
+    }
+}
