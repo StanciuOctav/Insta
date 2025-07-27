e5260e1be5276f8a290126171fae1d76a2ac416f
StanciuOctav <octav.stanciu00@gmail.com>
2025-07-27 18:08:59 +0300

    Created Xcode project

---
+++ Insta/Insta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Insta
+//
+//  Created by Octav Stanciu on 27.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Insta/Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by Octav Stanciu on 27.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
